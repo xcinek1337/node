b15f5e42475d27f0d984beb6bcb9201a84828bf6
Marcin <marcinchodkowski004@wp.pl>
2025-01-01 19:59:27 +0100

    Show who’s online

--- index.js
+++ index.js
@@ -6,17 +6,23 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+const online = [];
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
 	const nickname = socket.handshake.query.nickname;
+	online.push(nickname);
 
 	io.emit('onWelcome', nickname + ' join to the chat');
+	io.emit('addOnline', online);
 
 	socket.on('disconnect', () => {
 		io.emit('onDisconnect', nickname + ' left the chat');
+		online.pop(nickname)
+		io.emit('removeOnline', online);
 	});
 
 	socket.on('chat message', (message) => {
