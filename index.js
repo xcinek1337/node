0cc4f46ce4287ed3d283fe932bf869d02c895d98
Marcin Chdkowski <xcinek1337@wp.pl>
2025-01-02 20:54:11 +0100

    Add private messaging

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 const online = [];
+const userSockets = {};
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
@@ -14,19 +15,34 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
 	const nickname = socket.handshake.query.nickname;
+
 	online.push(nickname);
+	userSockets[nickname] = socket.id;
 
 	io.emit('onWelcome', nickname + ' join to the chat');
 	io.emit('addOnline', online);
 
 	socket.on('disconnect', () => {
 		io.emit('onDisconnect', nickname + ' left the chat');
-		online.pop(nickname)
+		online.pop(nickname);
 		io.emit('removeOnline', online);
 	});
 
 	socket.on('chat message', (message) => {
 		socket.broadcast.emit('chat message', { message: message, user: nickname });
+	});
+
+	socket.on('private message', (msgRecipient) => {
+		console.log('server');
+		
+		const { message, recipient } = msgRecipient;
+		const recipientSocketId = userSockets[recipient];
+
+		if (recipientSocketId) {
+			io.to(recipientSocketId).emit('private message', { message: message, sender: nickname });
+		} else {
+			socket.emit('userNotFound', recipient);
+		}
 	});
 
 	socket.on('isTyping', (nickname) => {
